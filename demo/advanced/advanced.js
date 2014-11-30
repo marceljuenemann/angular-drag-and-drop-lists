81272371f745c35bbe686ec9889a9daa8895067e
Marcel Juenemann <mail@marcel-juenemann.de>
2014-11-30 15:00:44 +0000

    Demo for #9

--- demo/advanced/advanced.js
+++ demo/advanced/advanced.js
@@ -3,23 +3,29 @@
  */
 angular.module("demo").controller("AdvancedDemoController", function($scope) {
 
+    $scope.dragoverCallback = function(event, index, external, type) {
+        $scope.logListEvent('dragged over', event, index, external, type);
+        return index > 0;
+    };
+
+    $scope.dropCallback = function(event, index, item, external, type, allowedType) {
+        $scope.logListEvent('dropped at', event, index, external, type);
+        if (external) {
+            if (allowedType === 'itemType' && !item.label) return false;
+            if (allowedType === 'containerType' && !angular.isArray(item)) return false;
+        }
+        return item;
+    };
+
     $scope.logEvent = function(message, event) {
         console.log(message, '(triggered by the following', event.type, 'event)');
         console.log(event);
     };
 
-    $scope.dragoverCallback = function(event, index, type) {
-        $scope.logEvent(type + ' element is dragged over position ' + index, event);
-        return index > 0;
-    };
-
-    $scope.dropCallback = function(event, index, item, type) {
-        $scope.logEvent(type + ' element was dropped at position ' + index, event);
-        if (type === 'itemType') {
-            if (item.label === 'Item 33') return false;
-            item.label += ' (dropped)';
-        }
-        return item;
+    $scope.logListEvent = function(action, event, index, external, type) {
+        var message = external ? 'External ' : '';
+        message += type + ' element is ' + action + ' position ' + index;
+        $scope.logEvent(message, event);
     };
 
     $scope.model = [];
