1cb7c69cb2ff72f71315f3a181301cabf197375d
Christopher Lenz <lenz@mediatis.de>
2015-03-06 10:44:40 +0100

    Don't stop propagation of click events on the draggable.
    I see no need to stop the propagation of click events.

--- angular-drag-and-drop-lists.js
+++ angular-drag-and-drop-lists.js
@@ -146,8 +146,6 @@ angular.module('dndLists', [])
         scope.$apply(function() {
           $parse(attr.dndSelected)(scope, {event: event});
         });
-
-        event.stopPropagation();
       });
 
       /**
