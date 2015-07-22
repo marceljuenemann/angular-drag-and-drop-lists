af907705aaed2661fc6bfcd1c2d284a290b1072f
Marcel Juenemann <mail@marcel-juenemann.de>
2015-07-22 23:11:06 +0100

    Stop preventing selectstart events, fixes #49

--- angular-drag-and-drop-lists.js
+++ angular-drag-and-drop-lists.js
@@ -164,7 +164,6 @@ angular.module('dndLists', [])
        */
       element.on('selectstart', function() {
         if (this.dragDrop) this.dragDrop();
-        return false;
       });
     };
   }])
