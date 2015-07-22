e7f2f5116fe14a6e1a52450713d0bc9f9331b4d2
Marcel Juenemann <mail@marcel-juenemann.de>
2015-07-22 22:52:32 +0100

    dnd-canceled and dnd-dragend callbacks, fixes #94, #39

--- angular-drag-and-drop-lists.js
+++ angular-drag-and-drop-lists.js
@@ -33,11 +33,16 @@ angular.module('dndLists', [])
    *                      remove your element from the original list in this callback, since the
    *                      directive is not doing that for you automatically. The original dragend
    *                      event will be provided in the local event variable.
+   * - dnd-canceled       Callback that is invoked if the element was dragged, but the operation was
+   *                      canceled and the element was not dropped. The original dragend event will
+   *                      be provided in the local event variable.
    * - dnd-copied         Same as dnd-moved, just that it is called when the element was copied
    *                      instead of moved. The original dragend event will be provided in the local
    *                      event variable.
    * - dnd-dragstart      Callback that is invoked when the element was dragged. The original
    *                      dragstart event will be provided in the local event variable.
+   * - dnd-dragend        Callback that is invoked when the drag operation ended. Available local
+   *                      variables are event and dropEffect.
    * - dnd-type           Use this attribute if you have different kinds of items in your
    *                      application and you want to limit which items can be dropped into which
    *                      lists. Combine with dnd-allowed-types on the dnd-list(s). This attribute
@@ -123,11 +128,14 @@ angular.module('dndLists', [])
             case "move":
               $parse(attr.dndMoved)(scope, {event: event});
               break;
-
             case "copy":
               $parse(attr.dndCopied)(scope, {event: event});
               break;
+            case "none":
+              $parse(attr.dndCanceled)(scope, {event: event});
+              break;
           }
+          $parse(attr.dndDragend)(scope, {event: event, dropEffect: dropEffect});
         });
 
         // Clean up
