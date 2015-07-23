e0b232e1f1d87919d162935bd10fc6b1da6ff880
Marcel Juenemann <mail@marcel-juenemann.de>
2015-07-23 01:28:23 +0100

    Bump version to 1.3.0

--- angular-drag-and-drop-lists.js
+++ angular-drag-and-drop-lists.js
@@ -1,5 +1,5 @@
 /**
- * angular-drag-and-drop-lists v1.2.0
+ * angular-drag-and-drop-lists v1.3.0
  *
  * Copyright (c) 2014 Marcel Juenemann mail@marcel-juenemann.de
  * Copyright (c) 2014-2015 Google Inc.
