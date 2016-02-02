dcf510e398d0733a133231c8c9cbb5b24773ad72
Marcel Juenemann <mail@marcel-juenemann.de>
2016-02-02 22:15:55 +0000

    Tests for dragstart event

--- test/dndDraggingSpec.js
+++ test/dndDraggingSpec.js
@@ -1,28 +1,82 @@
 describe('dndDragging', function() {
 
-  describe('dndDragging', function() {
-    var HTML = '<div dnd-draggable="{hello: world}"></div>';
+  var SIMPLE_HTML = '<div dnd-draggable="{hello: \'world\'}"></div>';
 
+  describe('constructor', function() {
     it('sets the draggable attribute', function() {
-      var element = compileAndLink(HTML);
+      var element = compileAndLink(SIMPLE_HTML);
+      expect(element.attr('draggable')).toBe('true');
+    });
+
+    it('watches and handles the dnd-disabled-if expression', function() {
+      var element = compileAndLink('<div dnd-draggable dnd-disable-if="disabled"></div>');
+      expect(element.attr('draggable')).toBe('true');
+
+      element.scope().disabled = true;
+      element.scope().$digest();
+      expect(element.attr('draggable')).toBe('false');
+
+      element.scope().disabled = false;
+      element.scope().$digest();
       expect(element.attr('draggable')).toBe('true');
     });
   });
 
-  describe('dnd-disable-if', function() {
-    var HTML = '<div dnd-draggable dnd-disable-if="disabled"></div>';
+  describe('dragstart', function() {
+    var element, event;
 
-    it('watches the expression and disables dragging', function() {
-      var element = compileAndLink(HTML);
-      expect(element.attr('draggable')).toBe('true');
-
-      $scope.disabled = true;
-      $scope.$digest();
-      expect(element.attr('draggable')).toBe('false');
-
-      $scope.disabled = false;
-      $scope.$digest();
-      expect(element.attr('draggable')).toBe('true');
+    beforeEach(function() {
+      element = compileAndLink(SIMPLE_HTML);
+      event = createEvent('dragstart');
     });
+
+    it('calls setData with serialized data', function() {
+      event._triggerOn(element);
+      expect(event._data).toEqual({'Text': '{"hello":"world"}'});
+    });
+
+    it('stops propagation', function() {
+      event._triggerOn(element);
+      expect(event._propagationStopped).toBe(true);
+    });
+
+    it('sets effectAllowed to move by default', function() {
+      event._triggerOn(element);
+      expect(event._dt.effectAllowed).toBe('move');
+    });
+
+    it('sets effectAllowed from dnd-effect-allowed', function() {
+      element = compileAndLink('<div dnd-draggable dnd-effect-allowed="copyMove"></div>');
+      event._triggerOn(element);
+      expect(event._dt.effectAllowed).toBe('copyMove');
+    });
+
+    it('adds CSS classes to element', inject(function($timeout) {
+      event._triggerOn(element);
+      expect(element.hasClass('dndDragging')).toBe(true);
+      expect(element.hasClass('dndDraggingSource')).toBe(false);
+
+      $timeout.flush(0);
+      expect(element.hasClass('dndDraggingSource')).toBe(true);
+    }));
+
+    it('invokes dnd-dragstart callback', function() {
+      element = compileAndLink('<div dnd-draggable dnd-dragstart="ev = event"></div>');
+      event._triggerOn(element);
+      expect(element.scope().ev).toBe(event.originalEvent);
+    });
+
+    it('initializes workarounds', inject(function(dndDropEffectWorkaround, dndDragTypeWorkaround) {
+      event._triggerOn(element);
+      expect(dndDragTypeWorkaround.isDragging).toBe(true);
+      expect(dndDragTypeWorkaround.dragType).toBeUndefined();
+      expect(dndDropEffectWorkaround.dropEffect).toBe('none');
+    }));
+
+    it('initializes workarounds respecting dnd-type', inject(function(dndDragTypeWorkaround) {
+      element = compileAndLink('<div dnd-draggable dnd-type="2 * 2"></div>');
+      event._triggerOn(element);
+      expect(dndDragTypeWorkaround.dragType).toEqual(4);
+    }));
   });
 });
--- test/init.js
+++ test/init.js
@@ -1,15 +1,46 @@
 var $compile,
-    $scope;
+    $rootScope;
 
 beforeEach(module('dndLists'));
 
 beforeEach(inject(function(_$compile_, _$rootScope_){
   $compile = _$compile_;
-  $scope = _$rootScope_;
+  $rootScope = _$rootScope_;
 }));
 
 function compileAndLink(html) {
-  var element = $compile(html)($scope);
-  $scope.$digest();
+  var scope = $rootScope.$new();
+  var element = $compile(html)(scope);
+  scope.$digest();
   return element;
+}
+
+function createEvent(type, dataTransfer) {
+  var dataTransfer = dataTransfer || {};
+  var event = {
+    originalEvent: {
+      dataTransfer: dataTransfer,
+      preventDefault: function() {
+        event._defaultPrevented = true;
+      },
+      stopPropagation: function() {
+        event._propagationStopped = true;
+      },
+    },
+    _data: {},
+    _dt: dataTransfer,
+    _triggerOn: function(element) {
+      // Retrieve event handlers from jQuery and invoke.
+      $._data($(element).get(0), "events")[type][0].handler(event);
+    },
+  };
+  switch (type) {
+    case 'dragstart':
+      dataTransfer.setData = function(type, value) {
+        event._data[type] = value;
+      };
+      break;
+  }
+
+  return event;
 }
