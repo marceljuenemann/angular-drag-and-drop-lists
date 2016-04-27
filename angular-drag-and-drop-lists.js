0a53163df34da9ebe3ffd30b2186a968dfea6c5c
Agnaldo Junior <agnaldo.junior01@gmail.com>
2016-04-27 01:07:39 -0400

    Fixed comments and mixed spaces

--- angular-drag-and-drop-lists.js
+++ angular-drag-and-drop-lists.js
@@ -485,23 +485,21 @@
 
       // See http://www.quirksmode.org/js/events_properties.html#position
       function getMousePosition(e) {
-      	if (!e) var e = window.event;
-		if (e.pageX || e.pageY) {
-			posx = e.pageX;
-			posy = e.pageY;
-		}
-		else if (e.clientX || e.clientY) {
-			posx = e.clientX + document.body.scrollLeft
-				+ document.documentElement.scrollLeft;
-			posy = e.clientY + document.body.scrollTop
-				+ document.documentElement.scrollTop;
-		}
-		return {x: posx, y: posy};
+        if (!e) var e = window.event;
+        if (e.pageX || e.pageY) {
+          posx = e.pageX;
+          posy = e.pageY;
+        }
+        else if (e.clientX || e.clientY) {
+          posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+          posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+        }
+        return {x: posx, y: posy};
       }
 
       function isMouseAtBeggining(event, targetNode, relativeToParent, sensitivity) {
-      	var mousePointer = horizontal ? getMousePosition(event).x : getMousePosition(event).y;
-      	var targetPosition = targetNode.getBoundingClientRect();
+        var mousePointer = horizontal ? getMousePosition(event).x : getMousePosition(event).y;
+        var targetPosition = targetNode.getBoundingClientRect();
         var targetPosition = horizontal ? targetPosition.left : targetPosition.top;
 
         return mousePointer < targetPosition + sensitivity;
@@ -509,7 +507,7 @@
 
       function isMouseAtEnd(event, targetNode, relativeToParent, sensitivity) {
         var mousePointer = horizontal ? getMousePosition(event).x : getMousePosition(event).y;
-      	var targetPosition = targetNode.getBoundingClientRect();
+        var targetPosition = targetNode.getBoundingClientRect();
         var targetPosition = horizontal ? targetPosition.right : targetPosition.bottom;
 
         return mousePointer > targetPosition - sensitivity;
