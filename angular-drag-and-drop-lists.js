87934e8d9eb19494041d8a493c1118e292a8e54e
ghobs91 <aghobrial@ptc.com>
2023-09-15 14:42:05 -0400

    rough version of indicator working

--- angular-drag-and-drop-lists.js
+++ angular-drag-and-drop-lists.js
@@ -317,9 +317,9 @@
         if (event.target != newTarget) {
           previousTarget = newTarget
           newTarget = event.target
-          previousTarget.parentNode.style.border = '1px solid #9aa5af'
+          previousTarget.parentNode.style.borderTop = 'none'
         }
-        console.log(`dragover event: ${event}`)
+        // console.log(`dragover event: ${event}`)
         // element.on('dragover', _.throttle(dragOver, 50, { leading: true, trailing: false }))
         // function dragOver(event){
         event = event.originalEvent || event;
@@ -342,7 +342,7 @@
           }
 
           if (listItemNode.parentNode == listNode && listItemNode != placeholderNode) {
-            console.log(`listItemNode.children: ${listItemNode.children} listItemNode.parentNode.children: ${listItemNode.parentNode.children}`)
+            // console.log(`listItemNode.children: ${listItemNode.children} listItemNode.parentNode.children: ${listItemNode.parentNode.children}`)
             // If the mouse pointer is in the upper half of the list item element,
             // we position the placeholder before the list item, otherwise after it.
             var rect = listItemNode.getBoundingClientRect();
@@ -353,17 +353,14 @@
             }
             if(isFirstHalf) {
               dragDropReplacementElement = listItemNode;
-              newTarget.style.borderTop = '3px solid blue';
+              newTarget.parentNode.style.borderTop = '3px solid blue';
               // console.log(`dragDropReplacementElement.children(): ${dragDropReplacementElement.children()}`);
               // listItemNode.css("border-top", "1px solid blue");
             } else {
               dragDropReplacementElement = listItemNode.nextSibling;
-              newTarget.style.borderTop = '3px solid blue';
+              newTarget.parentNode.style.borderTop = '3px solid blue';
             }
           }
-        }
-        else {
-          event.target.style.borderTop = '1px solid blue';
         }
 
         // In IE we set a fake effectAllowed in dragstart to get the correct cursor, we therefore
@@ -398,6 +395,7 @@
        */
       element.on('drop', function(event) {
         event = event.originalEvent || event;
+        newTarget.parentNode.style.borderTop = 'none'
 
         // Check whether the drop is allowed and determine mime type.
         var mimeType = getMimeType(event.dataTransfer.types);
